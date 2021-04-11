c28e249a75be8dfb7ddf9e8743d4abfb7870fd10
aidyn1984 <71663287+aidyn1984@users.noreply.github.com>
2021-04-10 22:13:54 -0500

    commit tree f7cfb49f18648dcbdeb54279a56b4bbb574a87ec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+p1 = Post.create(title: "Title Goes Here", description: "o you italicize book titles? Underline them? Put book titles in quotes? Here's the real scoop on how to handle styling book titles in your writing")
+p2 = Post.create(title: "I love Rails", description: "One of the great advantages of programming in Ruby is its resemblance to the English language. Ruby was designed to be intuitively read, and so it is common practice for advanced Ruby programmers to follow conventions of poetry mode.")
+p3 = Post.create(title: "My dog", description: "This past year, I became an aunt to my brother and sister-in-law’s dog, Remi. As the mother to two kitties, I’d always counted myself a cat person, but hanging out with Rem soon had me falling head over heels in love with this dog. Before long, I was #dogstagramming with dog selfies and everything, which got me searching for dog poems. Is “dog poetry” a thing? Oh, yeah!")
